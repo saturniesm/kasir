c14386bac036455941e025709db1ef1ae1a5410e
twiska nadila <107825842+twiskanadila@users.noreply.github.com>
2023-03-05 19:00:09 +0700

    all auth dome

--- kasir-backend/controllers/auth.controller.js
+++ kasir-backend/controllers/auth.controller.js
@@ -1,20 +1,35 @@
 const userModel = require("../models/index").user;
 const argon2 = require("argon2");
-const { validatePassword, validateRequiredFields, checkDuplicates, validateEmail } = require('../middleware/validation');
-const {generateAccessToken, generateRefreshToken } = require('../middleware/token');
+const jwt = require("jsonwebtoken");
+
+const {
+  validatePassword,
+  validateRequiredFields,
+  checkDuplicates,
+  validateEmail,
+} = require("../middleware/validation");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../middleware/token");
 
 // ROLE any user
 exports.register = async (request, response, next) => {
   try {
     const models = [userModel, userModel];
-    const fields = ['email', 'username'];
+    const fields = ["email", "username"];
 
-    request.requiredFields = ['nama_user', 'username', 'email', 'password', 'confPassword'];
+    request.requiredFields = [
+      "nama_user",
+      "username",
+      "email",
+      "password",
+      "confPassword",
+    ];
 
     checkDuplicates(models, fields)(request, response, () => {
       validateRequiredFields(request, response, () => {
         validatePassword(request, response, async () => {
-
           const { nama_user, username, email, password } = request.body;
           const hashPassword = await argon2.hash(password);
           const role = "undefined";
@@ -36,9 +51,9 @@ exports.register = async (request, response, next) => {
               username: user.username,
               email: user.email,
               role: user.role,
-              token: accessToken
+              token: accessToken,
             },
-            message: "Registrasi Berhasil",
+            message: "Registration success",
           });
         });
       });
@@ -49,70 +64,141 @@ exports.register = async (request, response, next) => {
 };
 
 // ROLE any user
-exports.login = async (req, res) => {
-    try {
-        req.requiredFields = ['email', 'password'];
-        validateRequiredFields(req, res, async () => {
-            validateEmail(req, res, async () => {
-                const { email, password } = req.body;
-                const user = await userModel.findOne({ where: { email } });
-                
-                if (!user) {
-                    return res.status(401).json({ message: 'Email does not exist' });
-                }
-                
-                const match = await argon2.verify(user.password, password);
-                
-                if (!match) {
-                    return res.status(401).json({ message: 'Wrong password' });
-                }
-                
-                const { username, email: userEmail, role, id_user } = user;
-                const accessToken = generateAccessToken(username, userEmail, role);
-                const refreshToken = generateRefreshToken(username, userEmail, role, id_user);
-            
-                res.cookie('refreshToken', refreshToken, {
-                    httpOnly: true,
-                    maxAge: 24 * 60 * 60 * 1000
-                });
-            
-                res.status(200).json({
-                    success: true,
-                    data: {
-                    username,
-                    email: userEmail,
-                    token: accessToken
-                    },
-                    message: 'Login Berhasil'
-                });
-            });
-        });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
- };
-  
-exports.logout = async (request, response) => {
-  const refreshToken = request.cookies.refreshToken;
-  if(!refreshToken) return response.sendStatus(204);
+exports.login = async (request, response) => {
+  try {
+    request.requiredFields = ["email", "password"];
+    validateRequiredFields(request, response, async () => {
+      validateEmail(request, response, async () => {
+        const { email, password } = request.body;
+        const user = await userModel.findOne({ where: { email } });
 
-  const user = await userModel.findOne({
-      where:{
-          refresh_token: refreshToken
-      }
-  })
-  if(!user) return response.sendStatus(204);
-  const id_user = user.id_user
-  await userModel.update({refresh_token: null},{
-      where:{
-          id_user: id_user
-      }
-  });
-  response.clearCookie('refreshToken');
-  return response.status(200).json({
-      'message': 'Logout'
-  })
+        if (!user) {
+          return response.status(401).json({ message: "Email does not exist" });
+        }
+
+        const match = await argon2.verify(user.password, password);
+
+        if (!match) {
+          return response.status(401).json({ message: "Wrong password" });
+        }
+
+        const { username, email: userEmail, role, id_user } = user;
+        const accessToken = generateAccessToken(username, userEmail, role);
+        const refreshToken = generateRefreshToken(
+          username,
+          userEmail,
+          role,
+          id_user
+        );
+
+        // TODO understanding this
+
+        await userModel.update(
+          { refresh_token: refreshToken },
+          {
+            where: { id_user: user.id_user },
+          }
+        );
+        response.cookie("refreshToken", refreshToken, {
+          httpOnly: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        });
+
+        response.status(200).json({
+          success: true,
+          data: {
+            username,
+            email: userEmail,
+            token: accessToken,
+          },
+          message: "Login success",
+        });
+      });
+    });
+  } catch (error) {
+    response.status(500).json({ message: "Internal server error" });
+  }
 };
 
+exports.logout = async (request, response) => {
+  try {
+    const refreshToken = request.cookies.refreshToken;
 
+    if (!refreshToken) {
+      return response.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await userModel.findOne({
+      where: {
+        refresh_token: refreshToken,
+      },
+    });
+
+    if (!user) {
+      return response
+        .status(401)
+        .json({ message: "Refresh token do not match" });
+    }
+
+    await userModel.update(
+      { refresh_token: null },
+      {
+        where: {
+          id_user: user.id_user,
+        },
+      }
+    );
+
+    response.clearCookie("refreshToken");
+
+    return response.status(200).json({
+      message: "Logout success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.handleRefreshToken = async (request, response) => {
+  try {
+    const refreshToken = request.cookies.refreshToken;
+    if (!refreshToken) {
+      return response.status(401).json({ message: "Refresh token not found" });
+    }
+    const user = await userModel.findOne({
+      where: {
+        refresh_token: refreshToken,
+      },
+    });
+
+    if (!user) {
+      return response
+        .status(403)
+        .json({ message: "Invalid or expired refresh token" });
+    }
+
+    try {
+      const { id_user, username, email, role } = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+      );
+
+      const accessToken = generateAccessToken(id_user, username, email, role);
+      response.json({ accessToken });
+    } catch (error) {
+      console.error(error);
+      response.status(403).json({ error: "Invalid refresh token" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/*
+
+Things that i can improve
+- Use express-validator (avoid sql injection that currently my own middleware dont cover etc)
+- Using a logger with winston
+- Is this even best practice including all in one file like this?
+- Create a constan for message
+*/
